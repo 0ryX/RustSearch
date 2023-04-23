cc1be4c345aaee111ca2c2f11c116a4afff777a5
Mandy Ison <mandyison@Mandys-MBP-186.lan>
2023-04-23 00:51:05 -0400

    compress lexer

--- src/main.rs
+++ src/main.rs
@@ -21,23 +21,34 @@ impl<'a> Lexer<'a> {
         }
     }
 
+    fn chop(&mut self, n: usize) -> &'a[char] {
+        let token = &self.content[0..n];
+        self.content = &self.content[n..];
+        token
+    }
+
+    fn chop_while<P>(&mut self, mut predicate: P) -> &'a [char] where P: FnMut(&char) -> bool {
+        let mut n = 0;
+        while n < self.content.len() && predicate(&self.content[n]) {
+            n += 1;
+        }
+        self.chop(n)
+    }
+
     fn next_token(&mut self) -> Option<&'a [char]> {
         self.trim_left();
         if self.content.len() == 0 {
             return None
         }
 
-        if self.content[0].is_alphabetic() {
-            let mut n = 0;
-            while n < self.content.len() && self.content[n].is_alphanumeric() {
-                n += 1;
-            }
-            let token = &self.content[0..n];
-            self.content = &self.content[n..];
-            return Some(token)
+        if self.content[0].is_numeric() {
+            return Some(self.chop_while(|x| x.is_numeric()));
         }
 
-        todo!()
+        if self.content[0].is_alphabetic() {
+            return Some(self.chop_while(|x| x.is_alphanumeric()));
+        }
+        return Some(self.chop(1));
     }
 }
 
@@ -55,12 +66,11 @@ fn index(content: &str) -> HashMap<String, usize> {
 fn read_entire_xml_file<P: AsRef<Path>>(file_path: P) -> io::Result<String> {
     let file = File::open(file_path)?;
     let er = EventReader::new(file);
-
     let mut content = String::new();
-
     for event in er.into_iter() {
         if let XmlEvent::Characters(text) = event.expect("TODO") {
-            content.push_str(&text); 
+            content.push_str(&text);
+            content.push_str(" "); 
         }
     }
     Ok(content)
@@ -70,7 +80,7 @@ fn main() -> io::Result<()>{
         .chars()
         .collect::<Vec<_>>();
     for token in Lexer::new(&content) {
-        println!("{token:?}");
+        println!("{token}", token = token.iter().map(|x| x.to_ascii_uppercase()).collect::<String>());
     }
     
 /*     let all_documents = HashMap::<Path, HashMap<Sring, usize>>new();
